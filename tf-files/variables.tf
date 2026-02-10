5b30277f0a5d4595b84b479be5b6d7e141281dad
babaktanriverdi <babaktanriverdi@gmail.com>
2026-02-10 22:03:34 +0100

    tf-files added

---
+++ tf-files/variables.tf
@@ -0,0 +1,19 @@
+variable "git-name" {
+  default = "<your git name>"
+}
+
+variable "git-token" {
+  default = "<your git token>"
+}
+
+variable "key-name" {
+  default = "<your key name>"
+}
+
+variable "instance-type" {
+  default = "c7i-flex.large" #!!!!!
+}
+
+variable "hosted-zone" {
+  default = "<your hosted zone>"
+}
